f78a02506b770b73fb131c291464b16ccf338fa7
apoyet <32321729+apoyet@users.noreply.github.com>
2018-06-07 14:11:10 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='MDOnlineAnalysis',
+      version='0.01',
+      description='This package provides several tools to monitor and post-process data online',
+      url='https://github.com/apoyet/BeamBeamTools',
+      author='Axel POYET',
+      author_email='axel.poyet@cern.ch',
+      license='GNU',
+      packages=['MDOnlineAnalysis'],
+zip_safe=False)
