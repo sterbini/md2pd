953b8a4f387a782a76fdef350e2c1730c3a070a5
apoyet <32321729+apoyet@users.noreply.github.com>
2018-06-07 14:11:40 +0200

    Update setup.py
    Correcting url

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='MDOnlineAnalysis',
       version='0.01',
       description='This package provides several tools to monitor and post-process data online',
-      url='https://github.com/apoyet/BeamBeamTools',
+      url='https://github.com/apoyet/MDOnlineAnalysis',
       author='Axel POYET',
       author_email='axel.poyet@cern.ch',
       license='GNU',
