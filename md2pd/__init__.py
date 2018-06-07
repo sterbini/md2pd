b3d3e707c87e8680b10d99b7fec6ceb60e72a4d3
apoyet <32321729+apoyet@users.noreply.github.com>
2018-06-07 16:08:35 +0200

    Update __init__.py

--- md2pd/__init__.py
+++ md2pd/__init__.py
@@ -1,1 +1,13 @@
+'''
+Suggested header for the notebook
 
+import md2pd
+from md2pd import monitorData
+from md2pd import postProcess
+
+cl2pd = monitorData.cl2pd
+cals=cl2pd.importData.cals
+pd = monitorData.pd
+np = monitorData.pd
+
+'''
