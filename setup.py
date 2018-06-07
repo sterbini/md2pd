ef858af0f21ecd044511ac71d166de381585be78
apoyet <32321729+apoyet@users.noreply.github.com>
2018-06-07 14:34:31 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='MDOnlineAnalysis',
+setup(name='md2pd',
       version='0.01',
       description='This package provides several tools to monitor and post-process data online',
-      url='https://github.com/apoyet/MDOnlineAnalysis',
+      url='https://github.com/apoyet/md2pd',
       author='Axel POYET',
       author_email='axel.poyet@cern.ch',
       license='GNU',
-      packages=['MDOnlineAnalysis'],
+      packages=['md2pd'],
 zip_safe=False)
